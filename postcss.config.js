bf40d7789d80f53b9790eb96670ec0c795800a8e
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2023-04-21 23:15:33 +0530

    rc

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
     // any other PostCSS plugins you may want to include
   ]
 }
